e16c1df3718de816d7596e2f554aae250ba31c4f
ofAbdala <159365509+ofAbdala@users.noreply.github.com>
2025-08-27 18:55:42 -0300

    Updated react-loadable-manifest.json

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-78c92fac7aa8fdd8.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8174a16db1f343bc.js","static/chunks/fd9d1056-7869610715a2008b.js","static/chunks/23-6f709caab0f687a5.js","static/chunks/main-app-325704efd855fe45.js"],pages:{"/_app":["static/chunks/webpack-8174a16db1f343bc.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-b82e25bb35541dde.js","static/chunks/pages/_app-6a626577ffa902a4.js"],"/_error":["static/chunks/webpack-8174a16db1f343bc.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-b82e25bb35541dde.js","static/chunks/pages/_error-1be831200e60c5c0.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e99dfcd3d8861a37"; }
+/******/ 		__webpack_require__.h = function() { return "24886f25e61ef613"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
