a6844e81365891b13eca2ee5baaa95a5ddf3b512
ofAbdala <159365509+ofAbdala@users.noreply.github.com>
2025-08-27 20:38:32 -0300

    Create .env.local with SUPERCELL_TOKEN

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b9eda1dd6500d05c"; }
+/******/ 		__webpack_require__.h = function() { return "9fed3589df83058a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.2c98766bc110b0b0.hot-update.js
+++ .next/static/webpack/webpack.2c98766bc110b0b0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7913687d02c3e5d2"; }
+/******/ 	__webpack_require__.h = function() { return "9fed3589df83058a"; }
 /******/ }();
 /******/ 
 /******/ }
