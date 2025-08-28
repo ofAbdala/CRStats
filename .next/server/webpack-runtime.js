68708c3139791918346dacbc771a203a124e2e4c
ofAbdala <159365509+ofAbdala@users.noreply.github.com>
2025-08-28 18:58:28 -0300

    Create .env.local file with SUPERCELL_TOKEN

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ed036443d9cc8a2b")
+/******/ 		__webpack_require__.h = () => ("74209fc2bb63acbe")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "abfcc94392cac63d"; }
+/******/ 		__webpack_require__.h = function() { return "58a88b13022bbe5e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
