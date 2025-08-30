866aa4e261d785b19a4e660953c0754b97443e70
ofAbdala <159365509+ofAbdala@users.noreply.github.com>
2025-08-29 22:38:20 -0300

    Fix Google Fonts AbortError

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "444aca18620562ed"; }
+/******/ 		__webpack_require__.h = function() { return "303aa7959516ba98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
